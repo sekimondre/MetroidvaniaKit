4abb51bb8dbca1d1943a2d38a9a0ee84b07b3433
sekimondre <sekimondre@icloud.com>
2025-01-27 18:11:21 -0300

    Removed commented wall grab condition

--- Sources/MetroidvaniaKit/Player/WallGrabState.swift
+++ Sources/MetroidvaniaKit/Player/WallGrabState.swift
@@ -6,19 +6,20 @@ class WallGrabState: PlayerState {
         player.velocity.x = 0
         player.velocity.y = 0
     }
+    
     func update(_ player: PlayerNode, dt: Double) -> PlayerState? {
         
         let direction = Input.getHorizontalAxis()
-//        if player.raycastForWall() {
-            if Input.isActionJustPressed(action: "ui_accept") {
-                player.velocity.y = Float(-player.getJumpspeed())
-                player.velocity.x = player.getWallNormal().sign().x * Float(player.speed) //* 0.25
-                player.wallJumpTimestamp = Time.getTicksMsec()
-                return JumpingState()
-            } else if Int(player.getWallNormal().sign().x) == Int(direction) {
-                return JumpingState()
-            }
-//        }
+        
+        if Input.isActionJustPressed(action: "ui_accept") {
+            player.velocity.y = Float(-player.getJumpspeed())
+            player.velocity.x = player.getWallNormal().sign().x * Float(player.speed) //* 0.25
+            player.wallJumpTimestamp = Time.getTicksMsec()
+            return JumpingState()
+        } else if Int(player.getWallNormal().sign().x) == Int(direction) {
+            return JumpingState()
+        }
+        
         return nil
     }
 }
