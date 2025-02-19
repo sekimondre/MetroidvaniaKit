e48a593e8b48835027e094d2322f0aa1b7a9026e
sekimondre <sekimondre@icloud.com>
2025-02-19 18:18:57 -0300

    Add flash effect & stunlock to enemies

--- Sources/MetroidvaniaKit/Enemies/Enemy.swift
+++ Sources/MetroidvaniaKit/Enemies/Enemy.swift
@@ -8,24 +8,52 @@ protocol EnemyAI {
 class Enemy: Node2D {
     
     @SceneTree(path: "AI") var enemyAI: EnemyAI?
+    @SceneTree(path: "Sprite2D") var sprite: Sprite2D?
     @SceneTree(path: "Hurtbox") var hurtbox: EnemyHurtbox?
     
     @Export var hp: Int = 10
     
+    var hitTimer = Timer()
+    var isStunlocked = false
+    
     override func _ready() {
+        hitTimer.autostart = false
+        hitTimer.oneShot = true
+        hitTimer.timeout.connect { [weak self] in
+            self?.flashTimeout()
+        }
+        addChild(node: hitTimer)
+        
         hurtbox?.onDamage = { [weak self] damage in
             self?.takeDamage(damage)
         }
     }
     
     override func _physicsProcess(delta: Double) {
+        guard !isStunlocked else { return }
         enemyAI?.update(self, delta: delta)
     }
     
     func takeDamage(_ amount: Int) {
+        flash()
         hp -= amount
         if hp <= 0 {
-            queueFree()
+            destroy()
         }
+    }
+    
+    func flash() {
+        isStunlocked = true
+        (sprite?.material as? ShaderMaterial)?.setShaderParameter(param: "flash_factor", value: Variant(1.0))
+        hitTimer.start(timeSec: 0.1)
+    }
+    
+    func flashTimeout() {
+        isStunlocked = false
+        (sprite?.material as? ShaderMaterial)?.setShaderParameter(param: "flash_factor", value: Variant(0.0))
+    }
+    
+    func destroy() {
+        queueFree()
     }
 }
