5330fdabf3556a8a19e1b951c40faf66147ca9ce
sekimondre <sekimondre@icloud.com>
2025-02-21 13:21:57 -0300

    Enemy hit player on area stay logic (reference retain)

--- Sources/MetroidvaniaKit/Enemies/EnemyHurtbox.swift
+++ Sources/MetroidvaniaKit/Enemies/EnemyHurtbox.swift
@@ -23,18 +23,34 @@ class EnemyHitbox: Area2D {
     
     @Export var damage: Int = 10
     
-//    var onDestroy: (() -> Void)?
+    private weak var playerHurtboxRef: PlayerHitbox?
     
     override func _ready() {
         collisionMask = 0b1_0000_0000
         
-        // TODO: if player stays in area it should take damage
         areaEntered.connect { [weak self] otherArea in
             guard let self, let otherArea else { return }
             if let playerHurtbox = otherArea as? PlayerHitbox {
-                let direction: Float = playerHurtbox.globalPosition < self.globalPosition ? -1.0 : 1.0
-                playerHurtbox.takeHit(damage, xDirection: direction)
+                self.playerHurtboxRef = playerHurtbox
             }
         }
+        
+        areaExited.connect { [weak self] otherArea in
+            guard let self, let otherArea else { return }
+            if let _ = otherArea as? PlayerHitbox {
+                self.playerHurtboxRef = nil
+            }
+        }
+    }
+    
+    override func _physicsProcess(delta: Double) {
+        if let playerHurtboxRef {
+            onAreaStay(playerHurtboxRef)
+        }
+    }
+    
+    func onAreaStay(_ playerHurtbox: PlayerHitbox) {
+        let direction: Float = playerHurtbox.globalPosition < self.globalPosition ? -1.0 : 1.0
+        playerHurtbox.takeHit(damage, xDirection: direction)
     }
 }
